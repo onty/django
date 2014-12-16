7586bb07f5d83210f0f2f3f75363727cad3e55aa
onty <lintang.prasojo@gmail.com>
2014-12-16 07:33:35 +0000

    customize admin page

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+class PageAdmin(Page):
+    list_display = ('title','category','url')
+    fields = ['title','category','url']
+    readonly_fields = ['title']
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page,PageAdmin)
