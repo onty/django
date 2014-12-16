cef182e39f5313658b980adce61be2fee27aa97d
Lintang Jati Prasojo <lintang@netkernel.theprasojos.web.id>
2014-12-16 17:07:21 +0000

    custom url

--- rango/admin.py
+++ rango/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-class PageAdmin(Page):
+class PageAdmin(admin.ModelAdmin):
     list_display = ('title','category','url')
     fields = ['title','category','url']
-    readonly_fields = ['title']
 
 admin.site.register(Category)
 admin.site.register(Page,PageAdmin)
--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,35 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+from rango.models import Category
+from rango.models import Page
 
 def index(request):
     context = RequestContext(request)
-    context_dict = {'boldmessage': "Bold Font from Context"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+#    context_dict = {'boldmessage': "Bold Font from Context"}
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
+
     return render_to_response('rango/index.html', context_dict, context)
 
 
 def about(request):
     return HttpResponse("About page, click <a href='/rango/'>here</a> to return")
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+    category_name = category_name_url.replace('_',' ')
+    context_dict = {'category_name': category_name}
+
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except category.DoesNotExist:
+        pass
+
+    return render_to_response('rango/category.html', context_dict, context)
+
