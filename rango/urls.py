60423d6521eec407672cc2827cadc6b73317502d
Lintang Jati Prasojo <lintang@netkernel.theprasojos.web.id>
2014-12-18 17:12:39 +0000

    part 7 not complete

--- rango/urls.py
+++ rango/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
         url(r'^about/', views.about, name='about'),
         url(r'^category/(?P<category_name_url>\w+)/$', views.category,
         name='category'),
+        url(r'^add_category/$', views.add_category, name='add_category'),
+        url(r'^add_page/$', views.add_page, name='add_page'),
 )
